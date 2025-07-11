0a6078b54a5af8ad389cebf534b4b534779c7e24
zhuyixiang <1339193686@qq.com>
2025-07-11 14:30:47 +0800

    add new

--- src/main/java/com/ftm/InvoiceCalc.java
+++ src/main/java/com/ftm/InvoiceCalc.java
@@ -35,8 +35,14 @@ public class InvoiceCalc {
 
                 output.append(String.format("FOB离岸价格%s%s，汇率%s，折合人民币%s;", order.getCurrency(), order.getTotalAmount(), currencyRate, order.getOffAmountCNY()));
                 output.append("\n");
-                output.append("单位:千克\n");
+                output.append("单位:千克； ");
                 output.append("合同号：" + order.getOrderId());
+                output.append("\n");
+
+                for (OrderDetail orderDetail : order.getOrderDetails()) {
+                    output.append(String.format("商品 %s, 总价 %s, 单位 %s", orderDetail.getProductName(), orderDetail.getInvoiceAmountCNY(), orderDetail.getUnit()));
+                    output.append("\n");
+                }
 
             } else if ("CIF".equalsIgnoreCase(order.getCjType())) {
                 BigDecimal allFeeCNY = order.getYfAmount().multiply(getCurrencyRate(order.getYfCurrency())).add(order.getBfAmount().multiply(getCurrencyRate(order.getBfCurrency()))).setScale(2, RoundingMode.HALF_UP);
@@ -59,9 +65,9 @@ public class InvoiceCalc {
 
                 output.append(String.format("CIF离岸价格%s%s，汇率%s，折合人民币%s;", order.getCurrency(), order.getTotalAmount(), currencyRate, order.getOffAmountCNY()));
                 output.append("\n");
-                output.append(String.format("其中:运费:%s%s: 保费:%s%s; 货款:%s%s，折合%s；", order.getYfCurrency(), order.getYfAmount(), order.getBfCurrency(), order.getBfAmount(), order.getCurrency(), order.getGoodsAmount(), order.getGoodsAmountCNY()));
+                output.append(String.format("其中:运费:%s%s: 保费:%s%s; 货款:%s%s，折合RMB%s；", order.getYfCurrency(), order.getYfAmount(), order.getBfCurrency(), order.getBfAmount(), order.getCurrency(), order.getGoodsAmount(), order.getGoodsAmountCNY()));
                 output.append("\n");
-                output.append("单位:千克\n");
+                output.append("单位:千克； ");
                 output.append("合同号：" + order.getOrderId());
                 output.append("\n");
 
@@ -69,8 +75,41 @@ public class InvoiceCalc {
                     output.append(String.format("商品 %s, 总价 %s, 单位 %s", orderDetail.getProductName(), orderDetail.getInvoiceAmountCNY(), orderDetail.getUnit()));
                     output.append("\n");
                 }
-            } else {
-                System.err.printf("unknown CJtype: %s%n", order.getOrderNo());
+            } else if ("C&F".equalsIgnoreCase(order.getCjType())) {
+                BigDecimal allFeeCNY = order.getYfAmount().multiply(getCurrencyRate(order.getYfCurrency())).setScale(2, RoundingMode.HALF_UP);
+                BigDecimal currencyRate = getCurrencyRate(order.getCurrency());
+                order.setOffAmountCNY(order.getTotalAmount().multiply(currencyRate.setScale(4, RoundingMode.HALF_UP)));
+                order.setCurrencyRate(currencyRate);
+                order.setGoodsAmountCNY(order.getOffAmountCNY().subtract(allFeeCNY).setScale(4, RoundingMode.HALF_UP));
+                order.setGoodsAmount(order.getGoodsAmountCNY().divide(currencyRate, 4, RoundingMode.HALF_UP));
+
+                order.setOffAmountCNY(order.getOffAmountCNY().setScale(2, RoundingMode.HALF_UP));
+                order.setGoodsAmount(order.getGoodsAmount().setScale(2, RoundingMode.HALF_UP));
+                order.setGoodsAmountCNY(order.getGoodsAmountCNY().setScale(2, RoundingMode.HALF_UP));
+                for (OrderDetail orderDetail : order.getOrderDetails()) {
+                    BigDecimal ratio = orderDetail.getUnit().divide(order.getTotalUnit(), 4, RoundingMode.HALF_UP);
+                    BigDecimal feeCny = allFeeCNY.multiply(ratio).setScale(4, RoundingMode.HALF_UP);
+                    orderDetail.setInvoiceAmountCNY(orderDetail.getAmount().multiply(currencyRate).subtract(feeCny).setScale(4, RoundingMode.HALF_UP));
+                    //orderDetail.setInvoicePriceCNY(orderDetail.getInvoiceAmountCNY().divide(orderDetail.getUnit(), 12, BigDecimal.ROUND_HALF_UP));
+                    orderDetail.setInvoiceAmountCNY(orderDetail.getInvoiceAmountCNY().setScale(2, RoundingMode.HALF_UP));
+                }
+
+                output.append(String.format("C&F离岸价格%s%s，汇率%s，折合人民币%s;", order.getCurrency(), order.getTotalAmount(), currencyRate, order.getOffAmountCNY()));
+                output.append("\n");
+                output.append(String.format("其中:运费:%s%s; 货款:%s%s，折合RMB%s；", order.getYfCurrency(), order.getYfAmount(), order.getCurrency(), order.getGoodsAmount(), order.getGoodsAmountCNY()));
+                output.append("\n");
+                output.append("单位:千克； ");
+                output.append("合同号：" + order.getOrderId());
+                output.append("\n");
+
+                for (OrderDetail orderDetail : order.getOrderDetails()) {
+                    output.append(String.format("商品 %s, 总价 %s, 单位 %s", orderDetail.getProductName(), orderDetail.getInvoiceAmountCNY(), orderDetail.getUnit()));
+                    output.append("\n");
+                }
+            }else {
+                System.err.printf("unknown CJtype: %s, %s", order.getOrderNo(), order.getCjType());
+                System.out.println();
+                System.out.println();
             }
 
             output.append("\n");
